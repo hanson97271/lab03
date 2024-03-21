1f15552cf4a28c06a8a72ea0172f29178bec68d0
hanson97271 <92702155+hanson97271@users.noreply.github.com>
2024-03-21 23:33:09 +0800

    Add files via upload

---
+++ JPA302.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class JPA302 {
+    public static void main(String[] args) {
+        int i=1,j=1,count=0;
+        for(i=1;i<=3;i++){
+            for(j=1;j<=9;j++){
+                count++;
+            }
+        }
+        System.out.printf("count=%d\n",count);
+    }
+}
